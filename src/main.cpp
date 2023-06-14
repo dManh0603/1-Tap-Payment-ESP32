9451ec84c832d7d9e897d4c0caad1389ff26388e
dmanh <dmanh6302@gmail.com>
2023-06-14 11:22:55 +0700

    fix api call

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,47 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h> // Include the ArduinoJSON library
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
 #define LED 25
 #define SS_PIN 5
-#define RST_PIN 22
+#define RST_PIN 32
 #define WIFI_SSID "121 dai la 2.4Ghz"
 #define WIFI_PASSWORD "12345689"
+#define SERVER_URL "http://192.168.1.10:5050/api/balance/deduct/"
+#define BUTTON_PIN 33
 
-MFRC522 rfid(SS_PIN, RST_PIN); // Create an MFRC522 instance
+MFRC522 rfid(SS_PIN, RST_PIN);      // Create an MFRC522 instance
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Initialize the LCD with the I2C address (0x27) and dimensions (16 columns, 2 rows)
+
+void displayMessage(String message)
+{
+  lcd.clear();         // Clear the LCD screen
+  lcd.setCursor(0, 0); // Set the cursor to the first column, first row
+  if (message != "Success")
+  {
+    lcd.print("Failed");
+    lcd.setCursor(0, 1);
+  }
+  lcd.print(message); // Print the message on the LCD
+  delay(2000);
+  lcd.clear();
+  lcd.print("Ready");
+}
 
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(LED, OUTPUT);
-  Serial.begin(115200); // Initialize serial communication
-  SPI.begin();          // Initialize SPI bus
-  rfid.PCD_Init();      // Initialize MFRC522
 
+  Serial.begin(115200); // Initialize serial communication
+
+  SPI.begin();     // Initialize SPI bus
+  rfid.PCD_Init(); // Initialize MFRC522
+
+  lcd.init();      // Initialize the LCD
+  lcd.backlight(); // Turn on the backlight
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
   while (WiFi.status() != WL_CONNECTED)
@@ -29,6 +53,7 @@ void setup()
 
   Serial.println("WiFi connected");
   Serial.println("Scan an RFID card");
+  displayMessage("hello world !!");
 }
 
 void loop()
@@ -58,8 +83,8 @@ void loop()
     if (WiFi.status() == WL_CONNECTED)
     {
       HTTPClient http;
-      String url = "http://192.168.1.10:5050/api/balance/deduct/" + uid;
-      
+      String url = SERVER_URL + uid;
+
       http.begin(url);
       http.addHeader("Content-Type", "application/json"); // Set the content type header for JSON
 
@@ -76,11 +101,27 @@ void loop()
       {
         String response = http.getString();
         Serial.println("Server response: " + response);
+        displayMessage("Success");
+      }
+      else if (httpResponseCode == 404)
+      {
+        String response = http.getString();
+        Serial.println("Server response: " + response);
+        displayMessage("Unrecognized");
+      }
+      else if (httpResponseCode == 400)
+      {
+        String response = http.getString();
+        Serial.println("Server response: " + response);
+        displayMessage("Declined");
       }
       else
       {
+        String response = http.getString();
         Serial.print("Error - HTTP response code: ");
         Serial.println(httpResponseCode);
+        Serial.println(response);
+        displayMessage("System failed");
       }
 
       http.end();
